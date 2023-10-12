5c690a0090981ae87de4a30e529c2fbac6c698fb
Matheus Lima <matheusmarinho.lima@hotmail.com>
2023-10-12 15:45:35 -0300

    Assinando token

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAspNet.Services;
 
@@ -7,6 +9,13 @@ public class TokenService
     public string Create()
     {
         var handler = new JwtSecurityTokenHandler(); // gerar o token
+
+        var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey); // ASCII - sem carcter especial
+        
+        // Assinando o token
+        new SigningCredentials(
+            new SymmetricSecurityKey(key), /* Metodo da chave simetrica onde passamos a chave */ 
+            SecurityAlgorithms.HmacSha256);
         
         var token = handler.CreateToken();
         return handler.WriteToken(token);
